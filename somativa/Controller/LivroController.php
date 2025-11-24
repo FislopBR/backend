463bad15ef537c18aa3b86553030cb736ce3e9e5
FislopBR <filippobrandt@gmail.com>
2025-11-24 15:10:00 -0300

    Add files via upload

---
+++ somativa/Controller/LivroController.php
@@ -0,0 +1,38 @@
+<?php
+require_once __DIR__ . '/../Model/LivroDAO.php';
+require_once __DIR__ . '/../Model/Livro.php';
+
+class LivroController {
+    private $dao;
+
+    // Construtor: cria o objeto DAO (responsável por salvar/carregar)
+    public function __construct() {
+        $this->dao = new LivroDAO();
+    }
+
+    // Lista todos os livros
+    public function criar($titulo, $autor, $ano, $genero, $qtde) {
+
+        // // Gera ID automaticamente com base no timestamp (exemplo simples)
+        // $id = time(); // Função caso o objeto tenha um atributo de ID
+
+        $livro = new livro( $titulo, $autor, $ano, $genero, $qtde);
+        $this->dao->criarLivro($livro);
+    } 
+    
+    public function ler() {
+        return $this->dao->lerlivro();
+    }
+  
+public function atualizar($tituloOriginal, $novotitulo, $autor, $ano, $genero, $qtde) {
+    $this->dao->atualizarlivros($tituloOriginal, $novotitulo, $autor, $ano, $genero, $qtde);
+}
+
+public function buscarPortitulo($nome) {
+    return $this->dao->buscarPortitulo($nome);
+}
+    // Exclui bebida
+    public function deletar($nome) {
+        $this->dao->excluirlivros($nome);
+    }
+}
